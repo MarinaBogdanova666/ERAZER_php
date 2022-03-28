facc39ec5d8c8c1bf087154ddd7d2eea680d5e52
kidwen-byte <79046116+kidwen-byte@users.noreply.github.com>
2022-03-28 20:18:28 +0400

    Поиск v1.1

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -4,17 +4,23 @@ namespace app\controllers;
 
 class SearchController extends Controller
 {
-    public function actionIndex() {
+    public function actionIndex()
+    {
         echo $this->render('search', ['page_size' => \App::getConfig('pageSize')]);
     }
+
     public function actionSearch($params)
     {
-        $item = \Products::getSearch($params['search']);
-        if (empty($item)) {
-            $item['message_error'] = "По вашему запросу {$params['search']} ничего не найдено";
-        }
+        $search = $params['search'];
+
+        $item = \Products::getSearch($search);
+        $message = [
+            'error' => "По вашему запросу {$search} ничего не найдено",
+            'success' => "Результаты запроса: {$search}"
+        ];
         echo $this->render('search', array_merge([
-            'item' => $item
+            'item' => $item,
+            'message' => $message
         ]));
     }
 }
